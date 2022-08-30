73f122e6db4df68b3c815d6b6ff6a6c78be29726
Hady Pratama <pratamaahady@gmail.com>
2022-08-30 23:50:48 +0700

    feat: API authentication route

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(['prefix' => 'auth','as'=>'auth.'], function(){
+    Route::post('login','AuthController@login')->name('login');
+    Route::delete('logout','AuthController@logout')->name('logout')->middleware(['auth:sanctum']);
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+    Route::resource('user','UserController');
+});
